73f1fd3edde036c9f58ac0f0228bcca429c739cd
Happy Yang <happy.yang@checkmarx.com>
2021-03-20 18:34:39 +0800

    ArrayDemo.java

--- JavaSampleCodeAndItsCxDOMStructure/ArrayDemo.java
+++ JavaSampleCodeAndItsCxDOMStructure/ArrayDemo.java
@@ -1,4 +1,6 @@
 /*
+* Sample code comes from https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html
+*
 * In this sample code, the following CxDOM Types are covered:
 * 4 ArrayCreateExpr
 * 6 ArrayInitializer
@@ -25,62 +27,70 @@
 * Please check ArrayDemo.png for the whole DOM tree of this file.
 */
 
-// Sample code comes from https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html
 
-// ArrayDemo | ClassDecl
 class ArrayDemo {
 
-    // Type References | TypeRefCollection,
-    // Members | MemberDeclCollection,
-
-    // main | MethodDecl
-    // void | TypeRef
-    // Parameter Declarations | ParamDeclCollection
-    // args | ParamDecl
-    // string | TypeRef
-    // [] | RankSpecifier
-    // Statements | StatementCollection
     public static void main(String[] args) {
 
-        // VariableDeclStmt | VariableDeclStmt
-        // int | TypeRef
-        // [] RankSpecifier
-        // anArray | Declarator
-        // null | NullLiteral
         // declares an array of integers
         int[] anArray;
 
-        // ExprStmt | ExprStmt
-        // AssignExpr | AssignExpr
-        // anArray | UnknownReference
-        // ArrayCreateExpr | ArrayCreateExpr
-        // 10 | IntegerLiteral
-        // ArrayInitializer | ArrayInitializer
-        // int | TypeRef
-        // [] | RankSpecifier
         // allocates memory for 10 integers
         anArray = new int[10];
 
-        // ExprStmt | ExprStmt
-        // AssignExpr | AssignExpr
-        // anArray | IndexerRef
-        // anArray | UnknownReference
-        // 0 | IntegerLiteral
-        // 100 | IntegerLiteral
         // initialize first element
         anArray[0] = 100;
 
-        // ExprStmt | ExprStmt
-        // println | MethodInvokeExpr
-        // println | MethodRef
-        // out | MemberAccess
-        // System | UnknownReference
-        // Param | Param
-        // BinaryExpr | BinaryExpr
-        // "Element at index 0: " | StringLiteral
-        // anArray | IndexerRef
-        // anArray | UnknownReference
-        // 0 | IntegerLiteral
         System.out.println("Element at index 0: " + anArray[0]);
     }
 }
+
+/*
+* source code element and its DOM Type:
+* ArrayDemo | ClassDecl
+*
+* Type References | TypeRefCollection
+* Members | MemberDeclCollection
+*
+* main | MethodDecl
+* void | TypeRef
+* Parameter Declarations | ParamDeclCollection
+* args | ParamDecl
+* String | TypeRef
+* [] | RankSpecifier
+* Statements | StatementCollection
+*	
+* VariableDeclStmt | VariableDeclStmt
+* int | TypeRef
+* [] RankSpecifier
+* anArray | Declarator
+* null | NullLiteral
+*
+* ExprStmt | ExprStmt
+* AssignExpr | AssignExpr
+* anArray | UnknownReference
+* ArrayCreateExpr | ArrayCreateExpr
+* 10 | IntegerLiteral
+* ArrayInitializer | ArrayInitializer
+* int | TypeRef
+* [] | RankSpecifier
+*
+* ExprStmt | ExprStmt
+* AssignExpr | AssignExpr
+* anArray | IndexerRef
+* anArray | UnknownReference
+* 0 | IntegerLiteral
+* 100 | IntegerLiteral
+*		
+* ExprStmt | ExprStmt
+* println | MethodInvokeExpr
+* println | MethodRef
+* out | MemberAccess
+* System | UnknownReference
+* Param | Param
+* BinaryExpr | BinaryExpr
+* "Element at index 0: " | StringLiteral
+* anArray | IndexerRef
+* anArray | UnknownReference
+* 0 | IntegerLiteral	
+*/
